f450cf9c122666a9a59561ba1c03026f342a4c98
Shabir Bhat <shabirbhat@simplyphi.com>
2018-06-13 17:21:26 +0530

    Created Weight Calculator

--- src/main/java/com/text/summarization/WeightCalculator.java
+++ src/main/java/com/text/summarization/WeightCalculator.java
@@ -1,46 +1,115 @@
 package com.text.summarization;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static com.text.summarization.Utils.*;
 
 public class WeightCalculator {
-//term freq
-    public double tf(List<String> doc, String term) {
-        double result = 0;
-        for (String word : doc) {
-            if (term.equalsIgnoreCase(word))
-                result++;
-        }
-        return result / doc.size();
+    private HashMap<String, Integer> documentFrequency = new HashMap<>();
+    private HashMap<String, Integer> termFrequency = new HashMap<>();
+
+    public WeightCalculator() throws IOException {
+        loadDfFile(RESOURCE_DIRECTORY + "dict.txt");
     }
-    //inverse doc freq
-    public double idf(List<List<String>> docs, String term) {
-        double n = 0;
-        for (List<String> doc : docs) {
-            for (String word : doc) {
-                if (term.equalsIgnoreCase(word)) {
-                    n++;
-                    break;
-                }
+
+    private void loadDfFile(String file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(file));
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            String[] split = s.split("\t");
+            if (split.length == 2) {
+                documentFrequency.put(split[0].trim(), Integer.valueOf(split[1].trim()));
+            } else {
+                System.out.println(s);
             }
         }
-        return Math.log(docs.size() / n);
+        scanner.close();
     }
+
+    //term freq
+    public void calculateTermFrequency(String[] vector) {
+        for (String vec : vector) {
+            termFrequency.put(vec, termFrequency.getOrDefault(vec, 0) + 1);
+        }
+    }
+
+    //inverse doc freq
+    public double idf(String term) {
+        if (documentFrequency.containsKey(term)) {
+            int docFreq = documentFrequency.get(term);
+            return Math.log(TOTAL_NO_OF_CORPUS / docFreq);
+        } else {
+            return 1.0;
+        }
+    }
+
     //term freq-inverse doc freq
-    public double tfIdf(List<String> doc, List<List<String>> docs, String term) {
-        return tf(doc, term) * idf(docs, term);
+    public double tfIdf(String term) {
+        return termFrequency.get(term) * idf(term);
     }
-    public static void main(String[] args) {
-        List<String> doc2 = Arrays.asList("another", "another", "eg", "eg", "eg" ) ;
-        List<String> doc1 = Arrays.asList("this", "is", "another", "another", "egaaaad");
-     //   List<String> doc3 = Arrays.asList("Has", "the", "disturb", "id", "sum");
-        List<List<String>> documents = Arrays.asList(doc1, doc2);
 
+    public static void main(String[] args) throws IOException {
+        OpenNlpTools openNlpTools = new OpenNlpTools();
+        HashMap<String, Integer> documentFreq = calculateDocFrequency(openNlpTools);
+        write(documentFreq);
         WeightCalculator calculator = new WeightCalculator();
-        double tfidf = calculator.tfIdf(doc2, documents, "eg");
-        System.out.println("TF-IDF (eg) = " + tfidf);
-
+        String[] tokens = "Calculates Df using background corpus".split(" ");
+        calculator.calculateTermFrequency(tokens);
+        System.out.println(calculator.tfIdf("Df"));
     }
 
+    /**
+     * Calculates Df using background corpus.
+     *
+     * @throws IOException
+     */
+    public static HashMap<String, Integer> calculateDocFrequency(OpenNlpTools openNlpTools) throws IOException {
+        int totalNoOfCorpusDocuments = 0;
+        HashMap<String, Integer> documentFrequency = new HashMap<>();
+        HashMap<Integer, Set<String>> documentss = new HashMap<>();
+        File dir = new File(RESOURCE_DIRECTORY + "Documents/");
+        File[] allfiles = dir.listFiles();
+        for (File f : allfiles) {
+            if (f.getName().endsWith(".txt")) {
+                String s = new String(Files.readAllBytes(Paths.get(f.getAbsolutePath())));
+                String document = s.toLowerCase();
+                Set<String> tokenSet = new HashSet<>();
+                String[] tokenArray = openNlpTools.getTokens(document);
+                String[] stems = openNlpTools.getStems(tokenArray);
 
+                for (int i = 0; i < tokenArray.length; i++) {
+                    String tok = tokenArray[i].trim().toLowerCase();
+                    if (!tokenSet.contains(tok)) {
+                        tokenSet.add(tok);
+                    }
+                    documentFrequency.put(tok, documentFrequency.getOrDefault(tok, 0) + 1);
+                }
+                for (int i = 0; i < stems.length; i++) {
+                    String stm = stems[i].trim().toLowerCase();
+                    if (!tokenSet.contains(stm)) {
+                        tokenSet.add(stm);
+                    }
+                    documentFrequency.put(stm, documentFrequency.getOrDefault(stm, 0) + 1);
+                }
+                documentss.put(totalNoOfCorpusDocuments++, tokenSet);
+            }
+        }
+        System.out.println("TOTAL_ CORPUS DOCUMENTS: " + totalNoOfCorpusDocuments);
+        return documentFrequency;
+    }
+
+    private static void write(HashMap<String, Integer> documentfreq) throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter(RESOURCE_DIRECTORY + "dict.txt");
+        Set<String> keys = documentfreq.keySet();
+        for (String key : keys) {
+            printWriter.write(key + "\t" + documentfreq.get(key) + "\n");
+        }
+        printWriter.close();
+    }
 }
